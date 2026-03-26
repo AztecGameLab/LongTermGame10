47b302a1711122b100afc734d9cba044d3d3268e
MisterMikah <mikahmart04@gmail.com>
2026-03-26 16:58:04 -0700

    Regenerate Level 3 and 4 and Added to heal_action.gd
    Level 3 (Antidote) • Chance to cure an ailment
    Level 4 • Heal Increase
      • Can heal Fortune past Max HP
    
    Both need some tweaks further along for curing ailment (which status effect) and also if max health should be only touched by regenerate.
    
    heal_action.gd • Added overheal boolean and if set to true, player can heal past max hp.

--- scripts/ability/action/heal_action.gd
+++ scripts/ability/action/heal_action.gd
@@ -2,7 +2,14 @@ extends TargetAction
 class_name HealAction
 
 @export var heal_amount: int
+@export var overheal: bool
 
 func run(context: ActionContext) -> void:
 	for target in resolve_targets(context):
-		BattleManager.apply_healing(heal_amount, context.source, target)
+		if overheal:
+			var overflow = maxi(0, target.current_health + heal_amount - target.max_health)
+			target.max_health += overflow
+			BattleManager.apply_healing(heal_amount, context.source, target)
+
+		else:
+			BattleManager.apply_healing(heal_amount, context.source, target)
