963f08a1f5a8ae508362d8835588e7571796ee8d
yellowstar396 <idanhaim2006@gmail.com>
2026-04-02 22:00:27 -0700

    Added DamageTakenBasedAction

--- scripts/status_effects/damage_taken_based_status_effect.gd
+++ scripts/status_effects/damage_taken_based_status_effect.gd
@@ -4,14 +4,28 @@ class_name DamageTakenBasedStatusEffect
 
 const DAMAGE_KEY = "stored_damage"
 
+## Type of damage recorded
+enum DamageType {
+	ORIGINAL_DAMAGE,
+	TRUE_DAMAGE,
+	FINAL_DAMAGE
+}
+
+## Whether this effect is positive, negative, or locked (cannot be transferred).
+@export var damage_type: DamageType = DamageType.TRUE_DAMAGE
+
 func _setup_container(container: StatusEffectContainer) -> void:
 	super._setup_container(container)
 	container.custom_state[DAMAGE_KEY] = 0.0
 
 func on_damage_received(context: AttackContext, container: StatusEffectContainer) -> void:
 	# Records the final damage value from the attack context
-	if container.custom_state.has(DAMAGE_KEY):
-		container.custom_state[DAMAGE_KEY] = context.final_damage 
+	if damage_type==DamageType.ORIGINAL_DAMAGE:
+		container.custom_state[DAMAGE_KEY] = context.original_damage
+	if damage_type==DamageType.TRUE_DAMAGE:
+		container.custom_state[DAMAGE_KEY] = context.true_damage
+	if damage_type==DamageType.FINAL_DAMAGE:
+		container.custom_state[DAMAGE_KEY] = context.damage
 
 func run_triggers(type: StatusEffectTrigger.Type, container: StatusEffectContainer) -> void:
 	# This allows the effect to fire actions (like StoredDamageAction)
