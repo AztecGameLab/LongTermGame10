93f991b2b833564a5cf3282b4a5ee7e2b5732fd4
af121school <aflores121school@gmail.com>
2026-04-29 13:41:52 -0700

    main menu

---
+++ scenes/levels/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _on_start_pressed() -> void:
+	print("Start pressed")
+
+
+func _on_continue_pressed() -> void:
+	print("Continue pressed")
+
+
+func _on_settings_pressed() -> void:
+	print("Settings pressed")
