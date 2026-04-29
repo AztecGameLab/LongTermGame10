7a406285b3dc9ef604c9e84457e11790903acbbe
Mystievous <mystievous@starseekstudios.com>
2026-04-29 14:28:48 -0700

    tweak and hook up main menu

--- scenes/levels/main_menu.gd
+++ scenes/levels/main_menu.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
+const level_1 := &"uid://cev3aj77jpniw"
+const credits := &"uid://bh8ipn2dy8ivj"
 
 func _on_start_pressed() -> void:
-	print("Start pressed")
+	get_tree().change_scene_to_file(level_1)
 
+func _on_games_pressed() -> void:
+	OS.shell_open("https://aztec-game-lab.itch.io/")
 
-func _on_continue_pressed() -> void:
-	print("Continue pressed")
-
-
-func _on_settings_pressed() -> void:
-	print("Settings pressed")
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file(credits)
