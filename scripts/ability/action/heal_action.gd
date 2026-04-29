299853c7ee4c4d32e0a90cae7d58dffba5e93fda
Mystievous <mystievous@starseekstudios.com>
2026-04-28 23:07:40 -0700

    remove overheal, add clear a bad status effect

--- scripts/ability/action/heal_action.gd
+++ scripts/ability/action/heal_action.gd
@@ -2,14 +2,7 @@ extends TargetAction
 class_name HealAction
 
 @export var heal_amount: int
-@export var overheal: bool
 
 func run(context: ActionContext) -> void:
 	for target in resolve_targets(context):
-		if overheal:
-			var overflow = maxi(0, target.current_health + heal_amount - target.max_health)
-			target.max_health += overflow
-			BattleManager.apply_healing(heal_amount, context.source, target)
-
-		else:
-			BattleManager.apply_healing(heal_amount, context.source, target)
+		BattleManager.apply_healing(heal_amount, context.source, target)
