24ab70fe481c54756167fe335863a1061718f9c1
Mystievous <mystievous@starseekstudios.com>
2026-04-30 23:37:19 -0700

    bosses 2 and 3

--- scripts/ability/action/apply_status_action.gd
+++ scripts/ability/action/apply_status_action.gd
@@ -18,5 +18,5 @@ class_name ApplyStatusAction
 func run(context: ActionContext) -> void:
 	for target in resolve_targets(context):
 		if status_effect:
-			print("applying status: " + str(context))
-			target.add_status_effect(status_effect, context.source, applied_stacks, max_stacks)
+			var stacks_to_apply := upgrade_stacks if target.has_status_effect(status_effect) else applied_stacks
+			target.add_status_effect(status_effect, context.source, stacks_to_apply, max_stacks)
--- scripts/ability/magic_cannon.gd
+++ scripts/ability/magic_cannon.gd
@@ -15,9 +15,10 @@ func get_label(_source: BattleCharacter) -> String:
 	return &"Magic Cannon"
 	
 func get_description(_source: BattleCharacter) -> String:
-	var description := &"Take one turn to charge the spell. Use it a second time to deal high damage."
+	var description := &"Take one turn to charge the spell. Use again for high damage."
+	description += &"\nWhen attacked, 50% chance to lose charge."
 	if has_extra_shots():
-		description += &"\nEvery turn you don't activate it, you gain one extra cannon shot worth a small amount of damage."
+		description += &"\nEvery turn not used, gain one charge worth extra damage."
 	return description
 
 func get_target_type(source: BattleCharacter) -> BaseAbility.TargetType:
